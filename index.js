aaed6f7f68d6ee0405037535955d73efe913086e
SolNegro <bruno.santos16264@a.fecaf.com.br>
2021-04-04 17:02:36 -0300

    Atualizacao 1

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const config = require('config');
-const PORT = process.env.PORT || config.get('server.port');
+const path = require('path');
+const port = process.env.PORT || config.get('server.port');
 
 const app = express();
 
-app.set('port', PORT);
+app.set('port', port);
 
 app.route('/extrato').get( (req, res) => {
     res.status(200).send("Você solicitou o Extrato")
@@ -14,6 +15,6 @@ app.route('/lancamento').get( (req, res) => {
     res.status(200).send("Você solicitou para adicionar um lancamento")
 } );
 
-app.listen(PORT, ()=> {
-    console.log("Servidor iniciado na porta: ", PORT);
+app.listen(port, ()=> {
+    console.log("Servidor iniciado na porta: ", port);
 })
